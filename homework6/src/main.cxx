d7d2a84f6719badebaa1a25701563ea27da0bc20
Sebastian Magnusson <sebbe.bebbe.11@gmail.com>
2024-10-11 14:27:43 +0200

    Updated for step 3

--- homework6/src/main.cxx
+++ homework6/src/main.cxx
@@ -1,30 +1,40 @@
 #include <iostream>
 #include <string>
 
+// Function computing the checksum, i.e. the sum of ASCII values of characters
+int computeCheckSum(const std::string& inputString) {
+    int checksum = 0;
+
+    // Range-based for-loop iterating over each character in the input string
+    for (char character : inputString) {
+        checksum += static_cast<int>(character);
+    }
+    return checksum;
+}
+
 int main(int argumentCount, char *arguments[]) {
+
     // Checks if the number of arguments is 3
     bool isCorrectArgumentCount = (argumentCount == 3);
 
     if (isCorrectArgumentCount) {
+
         // Descriptive names
         std::string programName{arguments[0]};
-        char firstArgument{*(arguments[1])};
+        char firstArgument{(arguments[1][0])};
         size_t programNameLength{programName.size()};
         int expectedResult{std::atoi(arguments[2])};
 
-        int asciiSum{0};
-        int currentIndex{0};
-
+        // Compute the checksum
         std::string inputString{arguments[1]};
+        int checksum = computeCheckSum(inputString);
 
-        while (true) {
-        asciiSum += inputString[currentIndex++];
-        if (currentIndex >= static_cast<int>(inputString.size())) {
-            break;
-        }
-        }
-        if ((asciiSum ^ firstArgument * 3) << (programNameLength & 0x1f) == expectedResult) {
-        std::cout << "Correct!" << std::endl;
+        // Moved oout of condition check for readability
+        int result = (checksum ^firstArgument * 3) << (programNameLength & 0x1f);
+
+        // Condition check
+        if (result == expectedResult) {
+            std::cout << "Correct!" << std::endl;
         } else {
         std::cout << "Wrong!" << std::endl;
         }
