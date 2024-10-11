0dc02b22c1d602a5e6c7f35da954fdd1d8f98133
Sebastian Magnusson <sebbe.bebbe.11@gmail.com>
2024-10-11 15:16:59 +0200

    Updated for step 5

--- homework6/src/main.cxx
+++ homework6/src/main.cxx
@@ -17,6 +17,18 @@ int calculateKey(int checksum, char firstArgument, size_t programNameLength) {
     return (checksum ^ (firstArgument * 3)) << (programNameLength & 0x1f);
 }
 
+void printUsageInstructions() {
+    std::cout << "Usage: ./program_name <input_string> <expected_key>\n";
+    std::cout << " <input_string> : String to be checked e.g. 'MNXB11'\n";
+    std::cout << " <expected_key> : The expected key e.g. '23552'\n";
+}
+
+void printResults(int checksum, int calculatedKey, int expectedKey) {
+    std::cout << "Calculated Checksum: " << checksum << "\n";
+    std::cout << "Calculated Key: " << calculatedKey << "\n";
+    std::cout << "Expected Key: " << expectedKey << "\n";
+    }
+
 int main(int argumentCount, char *arguments[]) {
 
     // Checks if the number of arguments is 3
@@ -34,8 +46,11 @@ int main(int argumentCount, char *arguments[]) {
         std::string inputString{arguments[1]};
         int checksum = computeCheckSum(inputString);
 
-        // Moved oout of condition check for readability
         int calculatedKey = calculateKey(checksum, firstArgument, programNameLength);
+
+        // Printing intermediate results
+        printResults(checksum, calculatedKey, expectedResult);
+
 
         // Condition check
         if (calculatedKey == expectedResult) {
@@ -43,5 +58,16 @@ int main(int argumentCount, char *arguments[]) {
         } else {
         std::cout << "Wrong!" << std::endl;
         }
-    }
+
+    // Prints usage instructions if the number of arguments =/ 3
+    } else {
+            std::cout << "Error: Invalid number of arguments.\n";
+            std::cout << "This program calculates a value by summing the ASCII values of characters from a given input string\n"
+                         "and compares the result to an expected value to determine if the input is correct.\n"
+                         "\n";
+                         
+            printUsageInstructions();
+            return 1; 
+        }
+    return 0;
 }
