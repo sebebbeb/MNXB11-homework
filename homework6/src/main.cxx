31be0c9f3e5a11c1256e202d61522d8b33905656
Sebastian Magnusson <sebbe.bebbe.11@gmail.com>
2024-10-11 14:02:25 +0200

    step 2 completion

--- homework6/src/main.cxx
+++ homework6/src/main.cxx
@@ -1,24 +1,32 @@
 #include <iostream>
 #include <string>
-int main(int var0, char *var1[]) {
-  if (var0 == 3) {
-    std::string var2{var1[0]};
-    auto var3{*(var1[1])};
-    auto var4{var2.size()};
-    auto var5{std::atoi(var1[2])};
-    auto var6{0};
-    auto var7{0};
-    std::string var8{var1[1]};
-    while (true) {
-      var6 += var8[var7++];
-      if (var7 >= static_cast<int>(var8.size())) {
-        break;
-      }
+
+int main(int argumentCount, char *arguments[]) {
+    // Checks if the number of arguments is 3
+    bool isCorrectArgumentCount = (argumentCount == 3);
+
+    if (isCorrectArgumentCount) {
+        // Descriptive names
+        std::string programName{arguments[0]};
+        char firstArgument{*(arguments[1])};
+        size_t programNameLength{programName.size()};
+        int expectedResult{std::atoi(arguments[2])};
+
+        int asciiSum{0};
+        int currentIndex{0};
+
+        std::string inputString{arguments[1]};
+
+        while (true) {
+        asciiSum += inputString[currentIndex++];
+        if (currentIndex >= static_cast<int>(inputString.size())) {
+            break;
+        }
+        }
+        if ((asciiSum ^ firstArgument * 3) << (programNameLength & 0x1f) == expectedResult) {
+        std::cout << "Correct!" << std::endl;
+        } else {
+        std::cout << "Wrong!" << std::endl;
+        }
     }
-    if ((var6 ^ var3 * 3) << (var4 & 0x1f) == var5) {
-      std::cout << "Correct!" << std::endl;
-    } else {
-      std::cout << "Wrong!" << std::endl;
-    }
-  }
 }
